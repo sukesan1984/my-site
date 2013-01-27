b1c199fe97b19823fcfc2179f22222e8f0e1ce2c
Kosuke Takami <takamikousuke@gmail.com>
2013-01-27 15:20:39 +0900

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MySite::Application.config.secret_token = 'aa04996856e84f4bfd42ceea95f9e7173ba3cdffea03077f0621a0e83d99992515b0de0de2f385c6313a946dc6cf6ad055bd7ef9177403575039ef2a32cd8295'
